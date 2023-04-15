a728b026fec656e481672906d2050b8d58e72a19
Vika Lubenets <lubenetcvika@gmail.com>
2023-04-15 20:40:15 +0400

    feat: add count cat solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,18 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  
+  if (sampleActivity == null || typeof(sampleActivity) !== 'string' || isNaN(sampleActivity)){
+    return false
+  }
+
+let num = parseFloat(sampleActivity);
+
+let result = Math.ceil((MODERN_ACTIVITY / num) / Math.floor(0.000122));
+
+return result
+
 }
 
 module.exports = {
