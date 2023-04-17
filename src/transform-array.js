00bd69c96c39107463a672b25f02093078c7650f
Vika Lubenets <lubenetcvika@gmail.com>
2023-04-17 20:33:38 +0400

    fix: fixed transform array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -25,9 +25,13 @@ function transform(arr) {
     if(arr[i] === '--discard-next'){
       i++;
     } else if (arr[i] === '--discard-prev'){
-      result.splice(-1, 1);
+      if(i - 1 > 0 && arr[i - 2] !== '--discard-next'){
+        result.splice(-1, 1);
+      }
     }else if (arr[i] === '--double-next'){
-      result.push(arr[i+1]);
+      if(i + 1 < arr.length){
+        result.push(arr[i+1])
+      };
     }else if (arr[i] === '--double-prev'){
       if(i - 1 >= 0 && arr[i - 2] !== '--discard-next') {
         result.push(arr[i-1]);
